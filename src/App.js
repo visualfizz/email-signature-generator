85f551f3822bf49bd8a7fbcf3cc6c18906a92eb5
Eric Castillo <eric.castillo@visualfizz.com>
2022-07-05 15:48:44 -0500

    Further development

--- src/App.js
+++ src/App.js
@@ -3,29 +3,30 @@ import "./App.css";
 import Header from "./Header";
 import Signature from "./Signature";
 import SignatureForm from "./SignatureForm";
-import { AppProvider } from './AppContext'
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
+import { AppProvider } from "./AppContext";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import HTMLMinifier from 'html-minifier-terser/dist/htmlminifier.esm.bundle';
 
 function App() {
-  const user = { name: 'First Last', job: "Job Title", phone: "555-123-4567" }
+  const user = { name: "First Last", job: "Job Title", phone: "555-123-4567" };
   const [loggedIn, setLoggedIn] = useState(false);
   const [username, setUsername] = useState(null);
   const [email, setEmail] = useState(null);
   const [showCopied, setShowCopied] = useState(false);
 
   useEffect(() => {
-    const credential = localStorage.getItem('credential');
+    const credential = localStorage.getItem("credential");
     if (credential) {
       fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${credential}`)
-        .then(res => res.json())
-        .then(response => {
+        .then((res) => res.json())
+        .then((response) => {
           setLoggedIn(true);
-          setUsername(response.name)
-          setEmail(response.email)
+          setUsername(response.name);
+          setEmail(response.email);
         })
-        .catch(error => console.log(error));
+        .catch((error) => console.log(error));
     }
-  }, []) // <-- empty dependency array
+  }, []); // <-- empty dependency array
 
   // const googleSuccess = async (res) => {
   //   console.log('auth.js-googlesuccess-res', res)
@@ -40,41 +41,47 @@ function App() {
   //   console.log('google signin failed-error', error)
   // }
 
-
-
   return (
     <GoogleOAuthProvider clientId="2231101338-jvngdfqalrv4aal892ujtr9j2sddngf9.apps.googleusercontent.com">
       <div className="App">
-        <Header loggedIn={loggedIn} onSignOut={signOut} username={username} email={email}></Header>
+        <Header
+          loggedIn={loggedIn}
+          onSignOut={signOut}
+          username={username}
+          email={email}
+        ></Header>
         <div className="container">
-          {!loggedIn &&
+          {!loggedIn && (
             <div id="signin-screen">
               <GoogleLogin
-                onSuccess={credentialResponse => {
+                onSuccess={(credentialResponse) => {
                   setLoggedIn(true);
-                  localStorage.setItem('credential', credentialResponse.credential);
-                  fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${credentialResponse.credential}`)
-                    .then(res => res.json())
-                    .then(response => {
+                  localStorage.setItem(
+                    "credential",
+                    credentialResponse.credential
+                  );
+                  fetch(
+                    `https://oauth2.googleapis.com/tokeninfo?id_token=${credentialResponse.credential}`
+                  )
+                    .then((res) => res.json())
+                    .then((response) => {
                       //console.log('user Info=', response)
-                      setUsername(response.name)
+                      setUsername(response.name);
                     })
-                    .catch(error => console.log(error));
+                    .catch((error) => console.log(error));
                 }}
                 onError={() => {
-                  console.log('Login Failed');
+                  console.log("Login Failed");
                 }}
               />
             </div>
-          }
-          {loggedIn &&
+          )}
+          {loggedIn && (
             <AppProvider value={user}>
               <div id="content" className="protected py-5">
                 <div className="row justify-content-center g-4">
                   <div className="col-lg-6">
-
                     <SignatureForm />
-
                   </div>
                   <div className="col-lg-6">
                     <div className="card h-100">
@@ -90,27 +97,39 @@ function App() {
                           >
                             Copy Signature
                           </button>
-                          {showCopied &&
-                            <div className={`text-success go`} onAnimationEnd={
-                              () => setShowCopied(false)
-                            }>Copied!</div>
-                          }
+                          {showCopied && (
+                            <div
+                              className={`text-success go`}
+                              onAnimationEnd={() => setShowCopied(false)}
+                            >
+                              Copied!
+                            </div>
+                          )}
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
+                <div className="row mt-5">
+                  <h2>How to Add the Signature to Your Email App</h2>
+                  <h3>Gmail</h3>
+                  <p><a href="https://support.google.com/mail/answer/8395" target="_blank">Create a Gmail Signature</a></p>
+                  <p>Paste the signature you created into the box.</p>
+                  <h3>Apple Mail (macOS)</h3>
+                  <p>Preferences > Signatures</p>
+                  <p>Click the + button to add a new signature. Uncheck the box "Always match my default message font." Paste the signature you created into the box.</p>
+                </div>
               </div>
             </AppProvider>
-          }
+          )}
         </div>
       </div>
-    </GoogleOAuthProvider >
+    </GoogleOAuthProvider>
   );
 
   function signOut() {
-    localStorage.removeItem('credential')
-    setLoggedIn(false)
+    localStorage.removeItem("credential");
+    setLoggedIn(false);
   }
 
   function copyToClip(str) {
@@ -124,12 +143,13 @@ function App() {
     document.removeEventListener("copy", listener);
   }
 
-  function copySignature() {
+  async function copySignature() {
+    var sigIframeHtml = document.getElementById("signature").contentWindow.document.documentElement.outerHTML;
+    var result = await HTMLMinifier.minify(sigIframeHtml, {collapseWhitespace: true, minifyCSS: true});
     copyToClip(
-      document.getElementById("signature").contentWindow.document
-        .documentElement.outerHTML
+      result
     );
-    setShowCopied(true)
+    setShowCopied(true);
   }
 }
 
--- src/Signature.js
+++ src/Signature.js
@@ -1,19 +1,29 @@
 import React, { useContext } from "react";
 import AppContext from "./AppContext";
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import sigHTML from 'raw-loader!./signature.html';
+import sigHTML from "raw-loader!./signature.html";
 
 const Signature = () => {
   const { user } = useContext(AppContext);
 
   function replaceText() {
-    var newSig = sigHTML.replace("{name}", user.name).replace("{job}", user.job);
-    if(user.phone) {
-      newSig = newSig.replace("{phone}", user.phone);
+    var newSig = sigHTML
+      .replace("{name}", user.name)
+      .replace("{job}", user.job);
+    if (user.phone) {
+      newSig = newSig.replace(/{phone}/g, formatPhoneNumber(user.phone));
     } else {
-      newSig = newSig.replace("{phone}", '&nbsp;').replace('P:', '');
+      newSig = newSig.replace(/{phone}/g, "&nbsp;").replace("P:", "");
     }
     return newSig;
+  }
+
+  function formatPhoneNumber(phoneNumber) {
+    const phoneDigits = phoneNumber.replace(/\D/g, "");
+    const phoneChunk1 = phoneDigits.slice(0, 3);
+    const phoneChunk2 = phoneDigits.slice(3, 6);
+    const phoneChunk3 = phoneDigits.slice(6, 10);
+    return `(${phoneChunk1}) ${phoneChunk2}-${phoneChunk3}`;
   }
 
   return (
@@ -23,4 +33,4 @@ const Signature = () => {
   );
 };
 
-export default Signature
+export default Signature;
